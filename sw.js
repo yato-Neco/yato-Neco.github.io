6129df5915a47a150e1790cd5fd7905866a360d8
x64neco <x64neco@gmail.com>
2022-04-17 01:26:36 +0900

    revision

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220417.0112';
+const cacheName = 'chirpy-20220417.0126';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
