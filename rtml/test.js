fe84a3509c36b234899862d083f2a8ebb80d2986
@g021c1169 <g021c1169@g.neec.ac.jp>
2022-07-19 00:05:22 +0900

    a

---
+++ rtml/test.js
@@ -0,0 +1,11 @@
+
+
+
+
+
+function ConversionButton(){
+    console.log("Hello");
+  
+
+}
+
--- rtml/wasm_test.d.ts
+++ rtml/wasm_test.d.ts
@@ -13,3 +13,35 @@ export function test(text: string): string;
 * @returns {string}
 */
 export function rtml(text: string): string;
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly greet: () => void;
+  readonly test: (a: number, b: number, c: number) => void;
+  readonly rtml: (a: number, b: number, c: number) => void;
+  readonly __wbindgen_add_to_stack_pointer: (a: number) => number;
+  readonly __wbindgen_malloc: (a: number) => number;
+  readonly __wbindgen_realloc: (a: number, b: number, c: number) => number;
+  readonly __wbindgen_free: (a: number, b: number) => void;
+}
+
+/**
+* Synchronously compiles the given `bytes` and instantiates the WebAssembly module.
+*
+* @param {BufferSource} bytes
+*
+* @returns {InitOutput}
+*/
+export function initSync(bytes: BufferSource): InitOutput;
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {InitInput | Promise<InitInput>} module_or_path
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function init (module_or_path?: InitInput | Promise<InitInput>): Promise<InitOutput>;
